0d61775d86a6295314bf67962a5f6ab85832eb83
V M Suchitra <93899964+SuchitraVM@users.noreply.github.com>
2026-05-06 12:06:24 +0530

    Create static form builder field layout

--- src/pages/BuilderPage.jsx
+++ src/pages/BuilderPage.jsx
@@ -1,35 +1,45 @@
 import { useState } from "react";
 
 export default function BuilderPage() {
-  const [fields, setFields] = useState([]);
+  // const [fields, setFields] = useState([]);
+  const [fieldLabel, setFieldLabel] = useState("");
 
-  function addFields(){
-    const newFields = {
-      id : Date.now(),
-      type : "text",
-      label :"New Field",
-    }
-    setFields([...fields, newFields]);
+  function handleLabelChange(e){
+    setFieldLabel(e.target.value);
   }
 
-  function updateFieldLabel(id, newLabel){
-    const updatedFields = fields.map((field) => {
-      field.id === id? {...field, label: newLabel} : field
-    });
-    setFields(updatedFields);
-  }
+  // function addFields(){
+  //   const newFields = {
+  //     id : Date.now(),
+  //     type : "text",
+  //     label :"New Field",
+  //   }
+  //   setFields([...fields, newFields]);
+  // }
+
+  // function updateFieldLabel(id, newLabel){
+  //   const updatedFields = fields.map((field) => {
+  //     field.id === id? {...field, label: newLabel} : field
+  //   });
+  //   setFields(updatedFields);
+  // }
 
   return (
     <div>
       <h2>Form Builder</h2>
-      <button onClick={addFields}>Add Field</button>
-
-      {
+      <button>Add Field</button>
+      <div>
+      <label >Field Label</label>
+      <input type="text" placeholder="Enter text here" value={fieldLabel} onChange={handleLabelChange}/>
+      <label >Preview Input</label>
+      <input type="text" placeholder="User input here" value={fieldLabel}/>
+      </div>
+      {/* 
         fields.map((field) => (
           <div key={field.id}>
             <input 
             type="text"
-            value={field.label}
+            value={<input value={field.label}/>}
             onChange={(e)=>
               updateFieldLabel(field.id, e.target.value)
             } 
@@ -37,7 +47,7 @@ export default function BuilderPage() {
             <input type="text" placeholder="User input here"/>
           </div>
         ))
-      }
+      } */}
 
    </div>
   )
