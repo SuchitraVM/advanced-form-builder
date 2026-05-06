0a9d1aac8ba1ea2f843a7da010f5223f8b81bedc
V M Suchitra <93899964+SuchitraVM@users.noreply.github.com>
2026-05-06 12:59:47 +0530

    Implemeny dynamic filed rendering and label editing

--- src/pages/BuilderPage.jsx
+++ src/pages/BuilderPage.jsx
@@ -1,53 +1,42 @@
 import { useState } from "react";
 
 export default function BuilderPage() {
-  // const [fields, setFields] = useState([]);
-  const [fieldLabel, setFieldLabel] = useState("");
+  const [fields, setFields] = useState([]);
+  
 
-  function handleLabelChange(e){
-    setFieldLabel(e.target.value);
+  function addFields(){
+    const newFields = {
+      id : Date.now(),
+      type : "text",
+      label : "field", 
+    }
+    setFields([...fields, newFields]);
   }
+  
 
-  // function addFields(){
-  //   const newFields = {
-  //     id : Date.now(),
-  //     type : "text",
-  //     label :"New Field",
-  //   }
-  //   setFields([...fields, newFields]);
-  // }
-
-  // function updateFieldLabel(id, newLabel){
-  //   const updatedFields = fields.map((field) => {
-  //     field.id === id? {...field, label: newLabel} : field
-  //   });
-  //   setFields(updatedFields);
-  // }
+  function updateFiledLabel(id , newLabel){
+    const updateFileds = fields.map((field)=>(
+      field.id === id ? {...field,label:newLabel} : field
+    ))
+    setFields(updateFileds);
+  }
 
   return (
     <div>
       <h2>Form Builder</h2>
-      <button>Add Field</button>
-      <div>
-      <label >Field Label</label>
-      <input type="text" placeholder="Enter text here" value={fieldLabel} onChange={handleLabelChange}/>
-      <label >Preview Input</label>
-      <input type="text" placeholder="User input here" value={fieldLabel}/>
-      </div>
-      {/* 
-        fields.map((field) => (
+      <button onClick={addFields}>Add Field</button>
+      {
+        fields.map((field=>(
           <div key={field.id}>
             <input 
             type="text"
-            value={<input value={field.label}/>}
-            onChange={(e)=>
-              updateFieldLabel(field.id, e.target.value)
-            } 
+            value={field.label}
+            onChange={(e)=>updateFiledLabel(field.id,e.target.value)}
             />
             <input type="text" placeholder="User input here"/>
           </div>
-        ))
-      } */}
+        )))
+      }
 
    </div>
   )
