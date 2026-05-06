1bcb43197ba30b4efd18f33fb02caf018ae97cbf
V M Suchitra <93899964+SuchitraVM@users.noreply.github.com>
2026-05-06 16:46:41 +0530

    Added conditional rendering to formType

--- src/pages/BuilderPage.jsx
+++ src/pages/BuilderPage.jsx
@@ -2,39 +2,61 @@ import { useState } from "react";
 
 export default function BuilderPage() {
   const [fields, setFields] = useState([]);
+  const [selectedFieldType, setSelectedFieldType] = useState("text")
   
 
   function addFields(){
     const newFields = {
       id : Date.now(),
-      type : "text",
-      label : "field", 
+      type : selectedFieldType,
+      label : "", 
     }
     setFields([...fields, newFields]);
   }
   
+ 
 
-  function updateFiledLabel(id , newLabel){
-    const updateFileds = fields.map((field)=>(
+  function updateFieldLabel(id , newLabel){
+    const updateFields = fields.map((field)=>(
       field.id === id ? {...field,label:newLabel} : field
     ))
-    setFields(updateFileds);
+    setFields(updateFields);
   }
+
+  //  function updateFieldType(id, newType){
+  //   const updateType = fields.map((field)=>(
+  //     filed.id===id ? {...field, type:newType} : field
+  //   ))
+  //   setSelectedFieldType(newType)
+  // }
 
   return (
     <div>
       <h2>Form Builder</h2>
       <button onClick={addFields}>Add Field</button>
+      <select value={selectedFieldType} onChange={(e)=>setSelectedFieldType(e.target.value)}>
+        <option value="text">text</option>
+        <option value="checkbox">checkbox</option>
+      </select>
       {
         fields.map((field=>(
+          field.type === "text" ?
           <div key={field.id}>
             <input 
-            type="text"
+            type={field.type}
             value={field.label}
-            onChange={(e)=>updateFiledLabel(field.id,e.target.value)}
+            onChange={(e)=>updateFieldLabel(field.id,e.target.value)}
             />
             <input type="text" placeholder="User input here"/>
+          </div> :
+          <div key={field.id}>
+            <label>
+            <input 
+            type={field.type}
+            />{field.label}
+            </label>
           </div>
+      
         )))
       }
 
